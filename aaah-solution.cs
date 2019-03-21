b1ab66c745a371b9f0247a9bb0a4e59d7b21b9b1
Garway Mei <35634860+Mayawan@users.noreply.github.com>
2019-03-20 23:46:16 -0400

    aaah completed

---
+++ aaah-solution.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aaah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Console.ReadLine().Length >= Console.ReadLine().Length) Console.WriteLine("go");
+            else Console.WriteLine("no");
+        }
+    }
+}
