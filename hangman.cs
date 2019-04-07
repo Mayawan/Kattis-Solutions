3cda105b44f38d617fe505987b22c32552d6f8f9
ArcherRival <markyangliu3@gmail.com>
2019-04-07 16:57:25 -0400

    Create hangman.cs

---
+++ hangman.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = Console.ReadLine();
+            String guesses = Console.ReadLine();
+            int counter = 0;
+
+            foreach (char c in guesses)
+            {
+                if (word == "")
+                {
+                    Console.WriteLine("WIN");
+                    break;
+                }
+
+                if(counter > 9)
+                {
+                    Console.WriteLine("LOSE");
+                    break;
+                }
+
+                if (word.Contains(c.ToString()))
+                {
+                    word = word.Replace(c.ToString(), "");
+                }
+                else counter++;
+            }
+        }
+    }
+}
