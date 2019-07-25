8e12981c78bf27ab9f30e7c39b89fb98bc3da2e8
Garway Mei <35634860+Mayawan@users.noreply.github.com>
2019-07-25 12:34:27 -0400

    stupid 1 liner time loop completed

---
+++ timeloop.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, int.Parse(Console.ReadLine())).ToList().ForEach(x => Console.WriteLine(x + " Abracadabra"));
+        }
+    }
+}
